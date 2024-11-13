6b63caf459ea193f331eb6ccb6cd78c636e2306b
Eclop14 <101784951+Eclop14@users.noreply.github.com>
2024-11-14 01:40:09 +0900

    11주차 [11월04일 - 11월10일]

---
+++ 11/11-1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* 
+    char* pChar;
+    int* pInt;
+    int count;
+
+    scanf("%d", &count);
+    pInt = (int*)malloc(count * sizeof(int));
+
+    for(int i = 0; i < count; i++) pInt[i] = rand() % 100;
+    for(int i = 0; i < count; i++) printf("%d ", pInt[i]);
+
+    pChar = (char*)pInt;
+
+    while(getchar() != '\n');
+    gets(pChar);
+    puts(pChar);
+
+    free(pInt);
+    return 0;
+    */
+    
+    
+    int* pArray;
+    char* pChar;
+    int count;
+
+    scanf("%d", &count);
+    pArray = (int*)malloc(sizeof(int) * count);
+
+    for(int i = 0; i < count; i++)
+        pArray[i] = i;
+
+    for(int i = 0; i < count; i++)
+        printf("%d ", pArray[i]);
+
+    pChar = (char*)pArray;
+
+    while(getchar() != '\n');
+    
+    gets(pChar);
+    puts(pChar);
+
+    return 0;
+}
+
+
+
